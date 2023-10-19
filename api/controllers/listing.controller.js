20786c181da75be8196b8bc69816e278bfa2011e
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-10-19 12:08:25 -0400

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from '../models/listing.model.js'
+import { errorHandler } from '../utils/errorHandler.js'
 
 
 export const createListing = async (req, res, next) => {
@@ -28,3 +29,25 @@ if(req.user.id !== listing.userRef) {
       next(error);
     }
 }
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if(!listing) {
+    return next(errorHandler(404, "Listing not found!"))
+  }
+  if(req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings!"))
+  }
+
+  try{
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new: true}
+      );
+      } catch (error) {
+        next(error);
+      }
+}
+
+
