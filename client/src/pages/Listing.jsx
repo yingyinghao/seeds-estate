8ec29d2c6335db2aa29510cc69da006348ad7e7f
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-10-23 16:42:54 -0400

    add contact landlord functionality to the liistinig page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -3,8 +3,18 @@ import { useParams } from "react-router-dom";
 import {Swiper, SwiperSlide} from "swiper/react";
 import SwiperCore from 'swiper';
 import { useSelector } from 'react-redux';
-import { Navigation } from 'swiper/navigation';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 
 
@@ -14,26 +24,29 @@ export default function Listing() {
   const [listing, setListing] = useState(null);
   const[loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
   const params = useParams();
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     const fetchListing = async () => {
       try{
         setLoading(true);
-      const res = await fetch(`/api/listing/get/${params.listingId}`);
-      const data = await res.json();
-      if(data.success === false){
-        setError(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if(data.success === false){
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
         setLoading(false);
-        return;
-      }
-      setListing(data);
-      setLoading(false);
-      setError(false);
-      }catch(error) {
-        setError(true)
-        setLoading(false);
-      }
+        setError(false);
+        }catch(error) {
+          setError(true)
+          setLoading(false);
+        }
     };
     fetchListing();
   },[params.listingId]);
@@ -59,7 +72,7 @@ export default function Listing() {
             </SwiperSlide>
           ))}
           </Swiper>
-            <div className="fixed top-[13%] right-[3%] z-10 border rounded-full">
+            <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
               <FaShare
                 className = 'text-slate-500'
                 onClick = {() => {
@@ -77,32 +90,32 @@ export default function Listing() {
               </p>
               )}
 
-  <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
-    <p className="text-2xl font -semibold">
-      {listing.name} - ${' '}
-      {listing.offer
-      ? listing.discountPrice.toLocaleString('en-US')
-      : listing.regularPrice.toLocaleString('en-US')}
-      {listing.type === 'rent' && '/mo'}
-    </p>
-    <p className="flex items-center mt-6 gap-2 text-slate-600 text-sm">
-  <FaMapMarkerAlt className='text-green-700' />
-  {listing.address}
-  </p>
-  <div className = "flex gap-4">
-    <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
-        {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
-    </p>
-    {listing.offer && (
-      <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
-        ${+listing.regularPrice - +listing.discountPrice} off
-      </p>
-    )}
-  </div>
-  <p className="text-slate-700">
-  <span className="font-semibold text-black">Description</span>
-  {listing.description}
-  </p>
+          <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+            <p className="text-2xl font-semibold">
+              {listing.name} - ${' '}
+              {listing.offer
+              ? listing.discountPrice.toLocaleString('en-US')
+              : listing.regularPrice.toLocaleString('en-US')}
+              {listing.type === 'rent' && '/mo'}
+            </p>
+            <p className="flex items-center mt-6 gap-2 text-slate-600 text-sm">
+          <FaMapMarkerAlt className='text-green-700' />
+          {listing.address}
+          </p>
+          <div className = "flex gap-4">
+            <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+            </p>
+            {listing.offer && (
+              <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                ${+listing.regularPrice - +listing.discountPrice} OFF
+              </p>
+            )}
+          </div>
+          <p className="text-slate-800">
+          <span className="font-semibold text-black">Description - </span>
+          {listing.description}
+          </p>
           <ul className="text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
             <li className="flex items-center gap-1 whitespace-nowrap">
               <FaBed className="text-lg"/>
