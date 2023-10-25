3ac5a963d4a360cf53624e27d81b401a28cd583a
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-10-25 15:14:36 -0400

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function About() {
   return (
-<div>About</div>
+<div className='py-20 px-4 max-w-6xl mx-auto'>
+  <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Ying Estate app
+  </h1>
+  <p className='mb-4 text-slate-700'>Ying Estate is a leading real estate agency that specializes in helping clients buy, sell and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceotional service and making the buying and selling process as smooth as possible. </p>
+  <p className='mb-4 text-slate-700'>We are a full-service real estate agency that can help you with all of your real estate needs. Whether you are looking to buy, sell or rent a property, we have the expertise and experience to help you find the perfect home for your family. Our agents are experts in their field and will work tirelessly to make sure that you get the best deal possible on your next home purchase or sale. </p>
+  <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+</div>
     )
 }
